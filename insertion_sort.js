100699c6c9dfdcba1071b88bc1d151564aa83b5e
andreskwan <andreskwan@gmail.com>
2016-06-04 07:29:49 -0500

    algorithms [insert sort - solved insertion] - problem was in the condition of the for loop, I should validate first "index >=0" because this avoids that the expression "array[index]" gets evaluated with a non valid index like "-1"

--- insertion_sort.js
+++ insertion_sort.js
@@ -1,45 +1,24 @@
-var slide = function(array, newPosition){
-    var value = array[newPosition - 1];
-    array[newPosition] = value;
-    return array;
-};
-
 var insert = function(array, rightIndex, value) {
-var index = 0;
-var previousValue = array[rightIndex];
-var lKey = array[rightIndex-1];
-var key = array[rightIndex+1];//value
-
-    for(index = rightIndex; value < array[index] && 0 <= index; index--) {
-        // print("b array: " + array+"\n");
-        // print("key: "+ key + " pv: "+ previousValue +"\n");
-        array = slide(array, index + 1);
-        previousValue = array[index-1];
-        // print("a array: " + array+"\n");
-        // array[index + 1] = key;
-        // key = array[index];
+	var index;
+	//first I need to be sure that index i is never less than 0;
+	//that is why it should come first in the and expression
+	//if evaluated then array[index] is never evaluated!!!
+	for(index = rightIndex; index >= 0 && value < array[index]; index--) {
+        array[index + 1] = array[index];
     }
-    array[index + 1] = value;    
-    // print("\n\n");
+    array[index + 1] = value;   
 };
-
-
-
-// var array = [10,7,3,13];
-// insert(array, 0, 7);
-// println("Array after inserting 7:  " + array);
-// Program.assertEqual(array, [7,10,3,13]);
 
 var array = [3, 5, 7, 11, 13, 2, 9, 6];
 
 insert(array, 4, 2);
 println("Array after inserting 2:  " + array);
-Program.assertEqual(array, [2, 3, 5, 7, 11, 13, 9, 6]);
+// Program.assertEqual(array, [2, 3, 5, 7, 11, 13, 9, 6]);
 
 insert(array, 5, 9);
 println("Array after inserting 9:  " + array);
-Program.assertEqual(array, [2, 3, 5, 7, 9, 11, 13, 6]);
+// Program.assertEqual(array, [2, 3, 5, 7, 9, 11, 13, 6]);
 
 insert(array, 6, 6);
 println("Array after inserting 6:  " + array);
-Program.assertEqual(array, [2, 3, 5, 6, 7, 9, 11, 13]);
+// Program.assertEqual(array, [2, 3, 5, 6, 7, 9, 11, 13]);
