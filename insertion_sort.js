48f48fdb93b0182463ec056060e9370a14464d84
andreskwan <andreskwan@gmail.com>
2016-06-04 08:21:42 -0500

    algorithms [insertionSort completed] - indexes are key and pay attention to the hints

--- insertion_sort.js
+++ insertion_sort.js
@@ -22,3 +22,14 @@ println("Array after inserting 9:  " + array);
 insert(array, 6, 6);
 println("Array after inserting 6:  " + array);
 // Program.assertEqual(array, [2, 3, 5, 6, 7, 9, 11, 13]);
+
+var insertionSort = function(array) {
+    for(var i = 1; i < array.length ; i++){
+        insert(array, i-1, array[i]);
+    }
+};
+
+var array = [22, 11, 99, 88, 9, 7, 42];
+insertionSort(array);
+println("Array after sorting:  " + array);
+Program.assertEqual(array, [7, 9, 11, 22, 42, 88, 99]);
